0968c1928b02ba5135a0759155a3050226b466ad
Pranav Ravuri <saibhushan@gmail.com>
2025-05-17 21:58:26 +0200

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in float" })
+vim.keymap.set("n", "<leader>cf", function()
+	require("conform").format({
+		lsp_format = "fallback",
+	})
+end, { desc = "Format current file" })
